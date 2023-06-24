766c9e5633afb5e941e2fbb60b2857701290fc70
Bruck Negash <brucknegash16@gmail.com>
2023-06-23 18:22:20 -0700

    Add files via upload

---
+++ src/dev/previews.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
+import {PaletteTree} from './palette'
+import App from "../App";
+import Landing from "../Pages/Landing";
+import Portfolio from "../components/Portfolio"
+import Home from "../components/Home";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/PaletteTree">
+                <PaletteTree/>
+            </ComponentPreview>
+            <ComponentPreview path="/App">
+                <App/>
+            </ComponentPreview>
+            <ComponentPreview path="/Landing">
+                <Landing/>
+            </ComponentPreview>
+            <ComponentPreview path="/Portfolio">
+                <Portfolio/>
+            </ComponentPreview>
+            <ComponentPreview path="/Home">
+                <Home/>
+            </ComponentPreview>
+        </Previews>
+    )
+}
+
+export default ComponentPreviews
