58856ebddf79004fe07fb90bde9b877ead7ccccf
Bruck Negash <brucknegash16@gmail.com>
2023-09-02 01:13:38 +0000

    updating

--- src/components/About.js
+++ src/components/About.js
@@ -2,31 +2,16 @@ import React from "react";
 import profilePic from "../images/profile-pic.jpg";
 import blogs from "../data/blogs.json";
 import "../styles/About.css";
+
 const About = () => {
     return (
         <div className="about-container">
-            <h1 className="about-title">About Me</h1>
+            <h1 className="about-title">About</h1>
             <div className="about-content">
-                <img src={profilePic} alt="Profile" className="about-profile" />
+                <img src={profilePic} alt="Profile" className="about-profile"/>
                 <p>
-                    Hi, I'm Bruck! I'm a software engineer
-                    working for the{" "}
-                    <a
-                        href="https://www.usekojo.com/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        City of Vernon as an IT-Intern
-                    </a>
-                    . Previously, I worked at{" "}
-                    <a
-                        href="https://www.lever.co/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Lever
-                    </a>
-                    . I recently graduated from{" "}
+                    Hi, I'm Bruck! I am an aspiring Full Stack Engineer,
+                    Developer. I recently graduated from {" "}
                     <a
                         href="https://www.calstatela.edu/"
                         target="_blank"
@@ -34,56 +19,15 @@ const About = () => {
                     >
                         California State University - Los Angeles
                     </a>{" "}
-                    with a degree in Computer Science
+                    with a B.S. in Computer Science
                 </p>
                 <p>
-                    In my free time I like to hike with my dog, cook, and read.
-                </p>
-                <p>
-                    I wrote this website almost entirely using{" "}
-                    <a
-                        href="https://openai.com/blog/chatgpt"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        ChatGPT
-                    </a>
-                    , with some help of{" "}
-                    <a
-                        href="https://reactjs.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        React
-                    </a>
-                    ,{" "}
-                    <a
-                        href="https://particles.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        react-tsparticles
-                    </a>
-                    , and{" "}
-                    <a
-                        href="https://reactrouter.com/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        react-router-dom
-                    </a>
-                    . I generated the portfolio banner images using{" "}
-                    <a
-                        href="https://openai.com/dall-e/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        DALL-E
-                    </a>
-                    .
+                    Aside from my love for all things tech,
+                    I also like to enjoy downtime by exercising, watching the movies, cooking,
+                    and exploring the world as much as I can.
                 </p>
 
-                <h2 className="about-subtitle">Blog Posts</h2>
+                <h2 className="about-subtitle">Tech Stack</h2>
                 <ul className="about-posts">
                     {blogs.map((blog) => (
                         <li key={blog.id}>
